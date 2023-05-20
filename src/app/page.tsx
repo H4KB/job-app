75829636d06bc73ce55957d4d17b2f37dff60ff9
H4KB <akabaharuka@gmail.com>
2023-05-20 18:06:17 +0900

    Create InputField component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 "use client";
 
 import { Button } from "@/components/button";
+import { InputField } from "@/components/form/input-field";
 import Image from "next/image";
 
 export default function Home() {
-  return <Button>Click me</Button>;
+  return (
+    <>
+      <Button>Click me</Button>
+      <br />
+      <InputField />
+    </>
+  );
 }
