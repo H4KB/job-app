1ed00d10588f788ff7e05f97d93d15bce0e54b73
H4KB <akabaharuka@gmail.com>
2023-06-27 20:32:08 +0900

    Replace to page router

---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import type { AppProps } from "next/app";
+import { NextPage } from "next";
+
+import { AppProvider } from "@/providers/app";
+import { ReactElement, ReactNode } from "react";
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+const App = ({ Component, pageProps }: AppPropsWithLayout) => {
+  const getLayout = Component.getLayout ?? ((page) => page);
+  const pageContent = getLayout(<Component {...pageProps} />);
+  return <AppProvider>{pageContent}</AppProvider>;
+};
+
+export default App;
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { Seo } from "@/components/seo/seo";
+import { Center } from "@chakra-ui/react";
+
+const LandingPage = () => {
+  return (
+    <>
+      <Seo title="Jobs App" />
+      <Center></Center>
+    </>
+  );
+};
+
+export default LandingPage;
