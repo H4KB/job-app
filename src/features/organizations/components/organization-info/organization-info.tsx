ca4705a77ca19227c8fb2ed87c3bfaee3512da80
H4KB <akabaharuka@gmail.com>
2023-06-27 22:45:16 +0900

    Create organization info component

--- src/features/organizations/components/organization-info/organization-info.tsx
+++ src/features/organizations/components/organization-info/organization-info.tsx
@@ -1,8 +1,31 @@
+import { Heading, Stack } from "@chakra-ui/react";
 import { Organization } from "../../types";
+import { InfoCard } from "@/components/info-card";
+import { Content } from "@/components/content";
 
 type OrganizationInfoProps = {
   organization: Organization;
 };
 export const OrganizationInfo = ({ organization }: OrganizationInfoProps) => {
-  return <></>;
+  return (
+    <>
+      <Stack
+        w="full"
+        alignItems="center"
+        justifyContent="space-between"
+        direction={{ base: "column", md: "row" }}
+      >
+        <Heading>{organization?.name}</Heading>
+        <Stack
+          w={{ base: "full", md: "auto" }}
+          direction={{ base: "column", md: "row" }}
+        >
+          <InfoCard label={organization.email} value={organization.email} />
+          <InfoCard label={organization.phone} value={organization.phone} />
+        </Stack>
+      </Stack>
+
+      <Content>{organization.info}</Content>
+    </>
+  );
 };
