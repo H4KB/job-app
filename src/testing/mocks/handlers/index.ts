829f4d8b972358bbcb498660c3241a3aa4db100a
H4KB <akabaharuka@gmail.com>
2023-07-04 00:54:28 +0900

    Create jobs handlers

--- src/testing/mocks/handlers/index.ts
+++ src/testing/mocks/handlers/index.ts
@@ -1,9 +1,11 @@
 import { API_URL } from "@/config/constants";
 import { rest } from "msw";
 import { authHandler } from "./auth";
+import { jobsHander } from "./jobs";
 
 export const handlers = [
   ...authHandler,
+  ...jobsHander,
   rest.get(`${API_URL}/healthcheck`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json({ healty: true }));
   }),
