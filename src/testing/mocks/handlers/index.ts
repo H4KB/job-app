fd9df66b5ad3df53bc09814c74674a3e66229a96
H4KB <akabaharuka@gmail.com>
2023-07-04 01:06:02 +0900

    Create organization handler

--- src/testing/mocks/handlers/index.ts
+++ src/testing/mocks/handlers/index.ts
@@ -2,10 +2,12 @@ import { API_URL } from "@/config/constants";
 import { rest } from "msw";
 import { authHandler } from "./auth";
 import { jobsHander } from "./jobs";
+import { organizationsHandler } from "./organizations";
 
 export const handlers = [
   ...authHandler,
   ...jobsHander,
+  ...organizationsHandler,
   rest.get(`${API_URL}/healthcheck`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json({ healty: true }));
   }),
--- src/testing/mocks/handlers/organizations.ts
+++ src/testing/mocks/handlers/organizations.ts
@@ -1,1 +1,25 @@
-export const organizationsHandlers = [];
+import { API_URL } from "@/config/constants";
+import { rest } from "msw";
+import { db } from "../db";
+
+const getOrganizationHandler = rest.get(
+  `${API_URL}/organizations/:organizationId`,
+  (req, res, ctx) => {
+    const organizationId = req.params.organizationId as string;
+    const organization = db.organization.findFirst({
+      where: { id: { equals: organizationId } },
+    });
+
+    if (!organization) {
+      return res(
+        ctx.delay(300),
+        ctx.status(404),
+        ctx.json({ message: "Not Found!" })
+      );
+    }
+
+    return res(ctx.delay(300), ctx.status(200), ctx.json(organization));
+  }
+);
+
+export const organizationsHandler = [getOrganizationHandler];
