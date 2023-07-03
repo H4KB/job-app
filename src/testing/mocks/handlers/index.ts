beae0218272ce129c39a9893de61446fe3399088
H4KB <akabaharuka@gmail.com>
2023-07-04 00:36:27 +0900

    Create auth handlers

--- src/testing/mocks/handlers/index.ts
+++ src/testing/mocks/handlers/index.ts
@@ -1,1 +1,10 @@
-export const handlers = [];
+import { API_URL } from "@/config/constants";
+import { rest } from "msw";
+import { authHandler } from "./auth";
+
+export const handlers = [
+  ...authHandler,
+  rest.get(`${API_URL}/healthcheck`, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({ healty: true }));
+  }),
+];
---
+++ src/testing/mocks/utils.ts
@@ -0,0 +1,60 @@
+import { AuthUser } from "@/features/auth";
+import { db } from "./db";
+import { RestRequest } from "msw";
+import { IS_TEST } from "@/config/constants";
+import { testData } from "../test-data";
+
+const AUTH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9";
+
+export const AUTH_COOKIE = "auth-token";
+
+const sanitizeUser = (user: any): AuthUser => {
+  const sanitizedUser = { ...user };
+  delete sanitizedUser.password;
+  return sanitizedUser;
+};
+
+export const authenticate = ({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) => {
+  const user = db.user.findFirst({
+    where: { email: { equals: email } },
+  });
+
+  if (user?.password === password) {
+    const sanitizedUser = sanitizeUser(user);
+    const encodedToken = AUTH_TOKEN;
+    return { user: sanitizedUser, jwt: encodedToken };
+  }
+
+  throw Error("Invalid username or password");
+};
+
+export const getUser = () => sanitizeUser(testData.users[0]);
+
+export const requireAuth = ({
+  req,
+  shouldThrow = true,
+}: {
+  req: RestRequest;
+  shouldThrow?: boolean;
+}): AuthUser | null => {
+  if (IS_TEST) {
+    return getUser();
+  }
+
+  const encodedToken = req.cookies[AUTH_COOKIE];
+
+  if (encodedToken !== AUTH_TOKEN) {
+    if (shouldThrow) {
+      throw new Error("No authorization token provided");
+    }
+    return null;
+  }
+
+  return getUser();
+};
