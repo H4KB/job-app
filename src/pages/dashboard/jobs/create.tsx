d15e908bd5c84ba1d44ccc6dac8d5de4790fe75b
H4KB <akabaharuka@gmail.com>
2023-08-14 17:54:35 +0900

    Show notification when created job

--- src/pages/dashboard/jobs/create.tsx
+++ src/pages/dashboard/jobs/create.tsx
@@ -1,13 +1,21 @@
 import { Seo } from "@/components/seo";
 import { CreateJobForm } from "@/features/jobs/components/create-job-form/create-job-form";
 import { DashBoardLayout } from "@/layouts/dashboard-layout";
+import { useNotifications } from "@/stores/notifications";
 import { Heading } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { ReactElement, ReactNode } from "react";
 
 const DashBoardCreateJobPage = () => {
   const router = useRouter();
+  const { showNotification } = useNotifications();
   const onSuccess = () => {
+    showNotification({
+      type: "success",
+      title: "Success",
+      message: "Job Created!",
+      duration: 5000,
+    });
     router.push("/dashboard/jobs");
   };
   return (
